781fb4eed8dba45a03ccc2f9ba846be7b7118091
Obnoxious <obnoxious@binhex.org>
2014-03-17 00:27:28 -0400

    Revert "Revert https://github.com/Obnoxious/nefarious2/commit/a41e6fbc8fe877ac89a718b5fb8e2f1acdf5e907"
    This reverts commit d0332a09ff8f7ed38f5cb1699581f405b7be31ad.

--- ircd/m_who.c
+++ ircd/m_who.c
@@ -221,7 +221,6 @@ int m_who(struct Client* cptr, struct Client* sptr, int parc, char* parv[])
           continue;
         case 'm':
         case 'M':
-         if (IsAnOper(sptr))
           matchsel |= WHO_FIELD_MRK;
           continue;
       }
